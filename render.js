6819c72c1db32f9e54b3e7b7ca3d7bd4fd6f4ff7
hdemon <zeitdiebe@gmail.com>
2017-12-30 13:47:53 +0900

    We can generate html from pug!

---
+++ render.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const marked = require('marked')
+const pug = require('pug')
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: (code) => require('highlight.js').highlightAuto(code).value,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+})
+
+const top = {
+  path: 'index.html',
+  templatePath: './top.pug',
+  data: {}
+}
+
+const articles = 
+  fs.readdirSync('./articles')
+    .map(path => ({
+      path: `articles/${path.replace(/\.md$/, '.html')}`,
+      templatePath: `articles/${path}`,
+      data: {}
+    }))
+
+const templates = [...articles, top]
+
+templates
+  .map(template => ({ path: `dist/${template.path}`, renderedString: pug.compileFile(template.templatePath)(template.data) }))
+  .forEach(page => fs.writeFileSync(page.path, page.renderedString))
